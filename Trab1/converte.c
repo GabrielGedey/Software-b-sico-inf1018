c906ee14c9c86f3835876cfbf321a351dc97c62a
Gabriel Augusto Gedey Porto <108944669+GabrielGedey@users.noreply.github.com>
2023-10-19 14:58:47 -0300

    Add files via upload

--- Trab1/converte.c
+++ Trab1/converte.c
@@ -14,6 +14,22 @@ int utf2varint(FILE *arq_entrada, FILE *arq_saida)
     if (ident == 30)
     {
       fread((carac+1), 1, 3, arq_entrada);
+      temp = carac[2] << 6;
+      carac[3] = carac[3] | temp;
+      carac[3] = carac[3] & 0x7F;
+      
+      carac[2] = carac[2] >> 1;
+      temp = carac[1] << 5;
+      carac[2] = carac[2] & 0x1F;
+      carac[2] = carac[2] | temp;
+      carac[2] = carac[2] | 0x80;
+      
+      carac[1] = carac[1] >> 2;
+      temp = carac[0] << 4;
+      carac[1] = carac[1] & 0x0F;
+      carac[1] = carac[1] | temp;
+      carac[1] = carac[1] | 0x80;
+      fwrite(carac+1, 1, 3, arq_saida);
     }
     else if (ident >= 28)
     {
@@ -22,12 +38,14 @@ int utf2varint(FILE *arq_entrada, FILE *arq_saida)
       carac[2] = carac[2] | temp;
       carac[2] = carac[2] & 0x7F;
       
-      temp = carac[0] << 6;
-      carac[1] = carac[1] | temp;
-      carac[1] = carac[1] & 0x7F;
-      
       carac[1] = carac[1] >> 1;
+      temp = carac[0] << 5;
       carac[1] = carac[1] & 0x1F;
+      carac[1] = carac[1] | temp;
+      carac[1] = carac[1] | 0x80;
+      
+      carac[0] = carac[0] >> 2;
+      carac[0] = carac[0] & 0x07;
       carac[0] = carac[0] | 0x80;
       fwrite(carac, 1, 3, arq_saida);
     }
@@ -35,12 +53,14 @@ int utf2varint(FILE *arq_entrada, FILE *arq_saida)
     {
       fread((carac+1), 1, 1, arq_entrada);
       temp = carac[0] << 6;
+      
       carac[1] = carac[1] | temp;
       carac[1] = carac[1] & 0x7F;
       
       carac[0] = carac[0] >> 1;
-      carac[0] = carac[0] & 0x0F;
+      carac[0] = carac[0] & 0x03;
       carac[0] = carac[0] | 0x80;
+      
       fwrite(carac, 1, 2, arq_saida);
       
     }
